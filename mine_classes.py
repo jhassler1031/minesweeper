a168bda4feb103653031061b594ed43c9a42d07a
jhassler1031 <jhassler1031@gmail.com>
2018-06-01 14:45:37 -0400

    board creation tells all nodes how many mines are adjacent, created played turn method

--- mine_classes.py
+++ mine_classes.py
@@ -12,21 +12,24 @@ import random
 #Start of Node class
 class Node:
     def __init__(self):
-        self.is_mine = False
+        self.is_mine = 0 #0 means not a mine, 1 means a mine
         self.adj_mines = 0
+        self.is_showing = False
 
     def make_mine(self):
-        self.is_mine = True
+        self.is_mine = 1
 
-    #Need way to count number of adjacent mines
-    def count_adj_mines(self):
-        pass
+    def make_showing(self):
+        self.is_showing = True
 
     def __repr__(self):
-        if self.is_mine:
-            return "X"
+        if self.is_showing:
+            if self.is_mine == 1:
+                return "M"
+            else:
+                return str(self.adj_mines)
         else:
-            return str(self.adj_mines)
+            return "X"
 
 #Start Board class
 class Board:
@@ -38,10 +41,44 @@ class Board:
             row = []
             for col in range(self.size):
                 new_node = Node()
-                if random.random() > .80:
+                if random.random() > .80: #20% chance of node being a mine
                     new_node.make_mine()
                 row.append(new_node)
             self.board.append(row)
+
+        #Need to give all nodes how many adj mines
+        board_size = self.size - 1
+
+        for r_idx, row in enumerate(self.board):
+            for c_idx, col in enumerate(row):
+                right = (row[c_idx + 1]).is_mine if c_idx < board_size else 0
+                left = (row[c_idx - 1]).is_mine if c_idx > 0 else 0
+                above = (self.board[r_idx - 1][c_idx]).is_mine if r_idx > 0 else 0
+                below = (self.board[r_idx + 1][c_idx]).is_mine if r_idx < board_size else 0
+
+                top_left = (self.board[r_idx -1][c_idx - 1]).is_mine if r_idx > 0 and c_idx > 0 else 0
+                top_right = (self.board[r_idx -1][c_idx + 1]).is_mine if r_idx > 0 and c_idx < board_size else 0
+                bottom_left = (self.board[r_idx + 1][c_idx - 1]).is_mine if r_idx < board_size and c_idx > 0 else 0
+                bottom_right = (self.board[r_idx + 1][c_idx + 1]).is_mine if r_idx < board_size and c_idx < board_size else 0
+
+                count = right + left + above + below + top_left + top_right + bottom_left + bottom_right
+                col.adj_mines = count
+
+    def player_turn(self):
+        while True:
+            player_input = input("Please enter the row and column number you would like to select, separated by a space: ")
+            player_input = player_input.split()
+            row = int(player_input[0])
+            col = int(player_input[1])
+            if row >= 0 and row < self.size:
+                if col >= 0 and col < self.size:
+                    break
+                else:
+                    print("Invalid column number")
+            else:
+                print("Invalid row number")
+
+        self.board[row][col].make_showing()
 
     def __str__(self):
         r_count = 0
@@ -67,15 +104,10 @@ class Board:
             print("")
             r_count += 1
         return ""
-"""
-    def __str__(self):
-        for row in self.board:
-            for col in row:
-                print(col, end = " ")
-            print("")
-        return ""
-"""
+
 #Testing ======================
 
 board = Board(5)
+print(board)
+board.player_turn()
 print(board)
